dfc46a8cc6de91e51528b88da193023d28c86842
Dharmaraj S <Dharmaraj.S@lntinfotech.com>
2022-03-23 21:59:02 +0530

    Fix:Polgon issue

--- adam/views.py
+++ adam/views.py
@@ -1,4 +1,6 @@
 import json
+import re
+from shapely.geometry import Point, Polygon
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from rest_framework.views import APIView
@@ -6,6 +8,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import AddressSerializers
 from .models import PanelMaster
+from geojson import Polygon
 from django.views.decorators.csrf import csrf_exempt
 from django.db import connection
 cursor = connection.cursor()
@@ -62,7 +65,8 @@ def view_address(request):
                     translate(panel_st.player_no,panel_st.code||'-','') as panel_st_panel_code
                     from adam_panelstaticdetails panel_st
                     join adam_panelmaster panel
-                    on panel.panel_no = translate(panel_st.player_no,panel_st.code||'-','') limit 100
+                    on panel.panel_no = translate(panel_st.player_no,panel_st.code||'-','') 
+                    and panel_st.city = 'N. Charleston' limit 100
                 '''
         cursor.execute(query)
         if (cursor.rowcount > 0):
@@ -120,16 +124,30 @@ def create_address(request):
 @csrf_exempt
 def check_area(request):
     if request.method == 'POST' and request.is_ajax():
+        # res = Polygon([[(-81.27058013661991, 35.294380959284894), (-81.19077879693572, 35.32694110417019), (-81.13713456303682, 35.30306494604871), (-81.2262460590176, 35.28026749345226)]])
+        # print("*************", res)
+        cods = request.POST.get('cods')
         min_lng = request.POST.get('min_lng')
         max_lng = request.POST.get('max_lng')
         min_lat = request.POST.get('min_lat')
         max_lat = request.POST.get('max_lat')
+        print(min_lng, max_lng)
         result = PanelMaster.objects.filter(longitude__range=(min_lng, max_lng)
                                         # latitude__range=(min_lat, max_lat)
                                         ).values('longitude', 'latitude', 'city', 'state', 'country')
-        print(list(result))
-        data = [{'longitude': '77.29972607572665', 'latitude': '13.200179182144382'}, {'longitude': '76.88224560697597', 'latitude': '13.028982684247879'}]
-        return HttpResponse(json.dumps({'status': "success", "data": list(result)}), content_type="application/json")
+        # print(list(result))
+        data = [{'longitude': '-79.955272', 'latitude': '32.837121', 'panel': '10710', 'market': 'charleston',
+                 'mediatype': 'Bulletin'},
+                {'longitude': '-79.978316', 'latitude': '32.851848', 'panel': '11520', 'market': 'charleston',
+                 'mediatype': 'Poster'},
+                {'longitude': '-79.986786', 'latitude': '32.847946', 'panel': 'P3716', 'market': 'charleston',
+                 'mediatype': 'Poster'},
+                {'longitude': '-79.967356', 'latitude': '32.841073', 'panel': '1245', 'market': 'charleston',
+                 'mediatype': 'Bulletin'},
+                {'longitude': '-79.944102', 'latitude': '32.797972', 'panel': '1012', 'market': 'charleston',
+                'mediatype': 'Bulletin'}]
+
+        return HttpResponse(json.dumps({'status': "success", "data": list(data)}), content_type="application/json")
     else:
         return HttpResponse(json.dumps({'status': "bad request"}), content_type="application/json")
 
