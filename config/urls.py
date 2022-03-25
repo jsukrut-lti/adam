4807e845a8cdef15b7a51fb1ad5dc47978515707
Tejeswar666-py <Guggella.Reddy@lntinfotech.com>
2022-03-25 19:21:57 +0530

    merging the urls and fix to edit-price icon hide

--- config/urls.py
+++ config/urls.py
@@ -19,24 +19,34 @@ from django.conf.urls import include, url
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
+from calc import views
+from django_plotly_dash import DjangoDash
+from django.views.generic import TemplateView
+from calc import analysis
 
 urlpatterns = [
     path('grappelli/', include('grappelli.urls')),  # grappelli URLS
     path('django_plotly_dash/', include('django_plotly_dash.urls')),
     path('admin/', admin.site.urls),
     path('', include('django.contrib.auth.urls')),
-    path('', include("calc.urls")),
+    path('', views.index),
     path('accounts/', include('allauth.urls')),
-    path('calculator/',include("calc.urls")),
-    path('analysis/',include("calc.urls")),
-    url(r'^login/$', auth_views.LoginView.as_view(),name='login'),
-    path('logout/', include("calc.urls")),
-    path('profile/', include("calc.urls")),
-    path('get_calculator_data/', include("calc.urls"), name = 'get_calculator_data'),
-    path('update_scenario_status/', include("calc.urls"), name = 'update_scenario_status')
+    path('home/', views.page_index, name='home'),
+    path('calculator/', views.calculator_view, name='calculator'),
+    path('analysis/', views.analysis_view, name='analysis'),
+    url(r'^login/$', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('profile/', views.get_profile, name='profile'),
+    path('financial-analysis/', views.financial_analysis_view, name='financial_analysis'),
+    path('financial-analysis/<str:key>/', views.financial_analysis_view, name='financial_analysis'),
+    path('financial-analysis-report/', views.financial_analysis_report, name='financial_analysis_report'),
+    path('financial-analysis-view-form/<str:key>/', views.financial_analysis_view_form,
+         name='financial-analysis-view-form'),
+    path('ajax/load-data/<str:key>/', views.load_data, name='ajax_load_data'),
+    path('get_calculator_data/', views.get_calculator_data, name='get_calculator_data'),
+    path('update_scenario_status/', views.update_scenario_status, name='update_scenario_status')
 ]
 
 if settings.DEBUG:
-	urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
-	urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
